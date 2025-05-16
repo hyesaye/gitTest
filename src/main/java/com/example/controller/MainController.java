18362bcbf5c95b32314851d707d65f22d4d23adf
choseohye <hyesaye@icloud.com>
2025-05-16 15:43:19 +0900

    log

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,8 +1,17 @@
 package com.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class MainController {
+	
+	Logger log = LoggerFactory.getLogger(getClass());
+	
+	@GetMapping(value = "/")
+	public String main() {
+	return "\"msg\":\"OK\"}";
+	}
 	
 	@GetMapping(value = "/list")
 	public String list() {
