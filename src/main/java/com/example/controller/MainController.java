071ba365bfc0510567b656a1a71c7f06cfcb9ea2
choseohye <hyesaye@icloud.com>
2025-05-16 15:59:12 +0900

    write

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class MainController {
 	
@@ -11,11 +14,16 @@ public class MainController {
 	@GetMapping(value = "/")
 	public String main() {
 	return "\"msg\":\"OK\"}";
-	}
+	} 
 	
 	@GetMapping(value = "/list")
 	public String list() {
 	return  "";
+	}
+	
+	@PostMapping(value = "/write")
+	public Map<String, Object> write(){
+		return null;
 	}
 
 }
